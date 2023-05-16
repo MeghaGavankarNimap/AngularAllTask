dfb4148751d42f828889444b17080aa7e299986c
MeghaGavankarNimap <meghagavankar@nimap.com>
2023-05-16 12:47:51 +0530

    Decorators

--- Decorators/README.md
+++ Decorators/README.md
@@ -1,4 +1,4 @@
-# Decorator
+# Decorators
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
--- Decorators/src/app/app.component.spec.ts
+++ Decorators/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Decorator'`, () => {
+  it(`should have as title 'Decorators'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Decorator');
+    expect(app.title).toEqual('Decorators');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Decorator app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Decorators app is running!');
   });
 });
